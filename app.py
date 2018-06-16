9c3afe41b7cef70b089ecd023b67049272614cbf
Nycholas de Sousa <nycholas@live.com>
2018-06-16 15:21:06 -0300

    User and Vehicle Register Forms; Login & Logout

--- app.py
+++ app.py
@@ -1,84 +1,122 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField, SelectField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy  import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:656993@localhost/flask_semob'
+app.config['SECRET_KEY'] = '123!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:656993@localhost/flask'
+bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean)
 
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    brand = db.Column(db.String(50), nullable=False)
-    model = db.Column(db.String(50), nullable=False)
-    plate = db.Column(db.String(50), unique=True, nullable=False)
-    date = db.Column(db.DateTime(), nullable=False)
-    reason = db.Column(db.String(50), nullable=False)
-    situation = db.Column(db.Boolean, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    brand = db.Column(db.String(30))
+    model = db.Column(db.String(30))
+    plate = db.Column(db.String(7))
+    date = db.Column(db.DateTime())
+    reason = db.Column(db.String(80))
+    situation = db.Column(db.Boolean)
 
-    def __init__(self, brand, model, plate, reason):
-        self.brand = brand
-        self.model = model
-        self.plate = plate
-        self.date = datetime.datetime.utcnow()
-        self.reason = reason
-        self.situation = True
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
+class LoginForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Length(min=4, max=50)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
-    admin = db.Column(db.Boolean, nullable=False)
-    vehicles = db.relationship('Vehicle', backref='user', lazy=True, nullable=True)
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    name = StringField('name', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
+class RegisterVehicle(FlaskForm):
+    brand = SelectField(
+        'brand',
+        choices=[('1', '1'), ('2', '2'), ('3', '3')]
+    )
+    model = SelectField(
+        'model',
+        choices=[('1', '1'), ('2', '2'), ('3', '3')]
+    )
+    plate = StringField('plate', validators=[InputRequired(), Length(max=7)])
+    reason = StringField('reason', validators=[InputRequired(), Length(max=80)])
 
-
-# Home Page
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Create Vehicle Page
-@app.route('/add_vehicle')
-def add_vehicle():
-    return render_template('add_vehicle.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
 
-# Create User Page
-@app.route('/create_user')
-def create_user():
-    return render_template('create_user.html')
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
 
-# POST User Method
-@app.route('/api/post/user', methods=['POST'])
-def post_user():
-    user = User(request.form['name'], request.form['email'], request.form['password'])
-    db.session.add(user)
-    db.session.commit()
+        return '<h1>Invalid email or password</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/registerVehicle', methods=['GET', 'POST'])
+def registerVehicle():
+    form = RegisterVehicle()
+    # form.brand.choices = [(row.ID, row.Name) for row in brand.query.all()]
+    # form.model.choices = [(row.ID, row.Name) for row in model.query.all()]
+
+    if form.validate_on_submit():
+        new_vehicle = Vehicle(brand=form.brand.data, model=form.model.data, plate=form.plate.data, date=datetime.datetime.utcnow(), reason=form.reason.data, situation=True)
+        db.session.add(new_vehicle)
+        db.session.commit()
+        return 'Vehicle Added'
+
+    return render_template('registerVehicle.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(name=form.name.data, email=form.email.data, password=hashed_password, admin=False)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return '<h1>New user has been created!</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.name)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
     return redirect(url_for('index'))
-
-# POST Vehicle Method
-@app.route('/api/post/vehicle', methods=['POST'])
-def post_vehicle():
-    vehicle = Vehicle(request.form['brand'], request.form['model'], request.form['plate'], request.form['reason'])
-    db.session.add(vehicle)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-
-# GET Vehicle Info Method
-@app.route('/api/get/vehicle/<plate>', methods=['GET'])
-def get_vehicle(plate):
-    vehicle = Vehicle.query.filter_by(plate=plate).first()
-    return render_template('vehicle_info.html', vehicle=vehicle)
-
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
